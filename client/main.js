ff2968fece65bb60c94a8b852643916c61486c59
jonabasque <jonmultimedia@gmail.com>
2016-06-03 12:09:10 +0200

    Cambio a sintaxis de componetes (en Angular 1.X)

--- client/main.js
+++ client/main.js
@@ -2,16 +2,18 @@ import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 
 angular.module('liquidpoll', [ angularMeteor ])
-	.controller('PartidosListCtrl', function($scope, $reactive) {
-		'ngInject';
-		$reactive(this).attach($scope);
-		
-		this.helpers({
-			partidos() {
-				return Partidos.find({});
-			}
+	.component('partidosList', {
+		templateUrl: 'client/partials/partidosList.html',
+    		controllerAs: 'partList',
+		controller($scope, $reactive) {
+      			'ngInject';
+      			$reactive(this).attach($scope);
+			this.helpers({
+				partidos() {
+          				return Partidos.find({});
+				}
+      			});
 
-    		});
-	});
+    		}
 
-
+  	});
