7c529f1324e89c0616b68b1797662bcbc1008f7e
jonabasque <jonmultimedia@gmail.com>
2016-06-03 11:11:43 +0200

    Cambio de sintaxis a ControllerAs en vistas y añadidos  y 'ngInject' en controllers

--- client/main.js
+++ client/main.js
@@ -2,14 +2,16 @@ import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 
 angular.module('liquidpoll', [ angularMeteor ])
-	.controller('PartidosListCtrl', ['$scope', function($scope) {
-
-		$scope.helpers({
+	.controller('PartidosListCtrl', function($scope, $reactive) {
+		'ngInject';
+		$reactive(this).attach($scope);
+		
+		this.helpers({
 			partidos() {
 				return Partidos.find({});
 			}
 
     		});
-	}]);
+	});
 
 
