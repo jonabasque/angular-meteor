47d06ff7a10e4b788d092e12e57a712f90aad38a
jonabasque <jonmultimedia@gmail.com>
2016-06-04 11:55:17 +0200

    Creado componente Navigation e incluido como dependencia en LiquidPoll

--- imports/ui/components/liquidPoll/liquidPoll.js
+++ imports/ui/components/liquidPoll/liquidPoll.js
@@ -4,13 +4,14 @@ import uiRouter from 'angular-ui-router';
 
 import template from './liquidPoll.html';
 import { name as PartidosList } from '../partidosList/partidosList';
+import { name as Navigation } from '../navigation/navigation';
 
 class LiquidPoll {}
 
 const name = 'liquidPoll';
 
 // create a module
-export default angular.module( name, [ angularMeteor, PartidosList, uiRouter ])
+export default angular.module( name, [ angularMeteor, PartidosList, Navigation, uiRouter ])
   .component(name, {
     template,
     controllerAs: name,
