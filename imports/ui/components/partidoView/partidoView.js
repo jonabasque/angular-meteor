d9bf3134a5f9c7e837186777e3944ff48a0753ae
jonabasque <jonmultimedia@gmail.com>
2016-06-05 10:48:42 +0200

    Completado componente PartidoView

--- imports/ui/components/partidoView/partidoView.js
+++ imports/ui/components/partidoView/partidoView.js
@@ -3,11 +3,45 @@ import angularMeteor from 'angular-meteor';
 import uiRouter from 'angular-ui-router';
 
 import template from './partidoView.html';
+import { Partidos } from '../../../api/partidos';
 
 class PartidoView {
-  constructor($stateParams) {
+  constructor($stateParams, $scope, $reactive) {
     'ngInject';
+    $reactive(this).attach($scope);
+
     this.partidoId = $stateParams.partidoId;
+
+    this.helpers({
+      //TODO: Entender cuando se ejecuta partido() para disponer de los datos.
+      //Parece que los helpers se cran al proncipio, por eso se ponen el el construct()
+      partido() {
+        return Partidos.findOne({
+          _id: $stateParams.partidoId
+        });
+      }
+    });
+  }
+
+  save() {
+    console.log('Ejecutado método save de helpers')
+    Partidos.update(
+      {
+        _id: this.partido._id
+      },
+      {
+        $set: {
+          name: this.partido.name,
+          description: this.partido.description
+        }
+      }, (error) => {
+        if (error) {
+          console.log('Oops, unable to update the party...');
+        } else {
+          console.log('Done!');
+        }
+      }
+    );
   }
 }
 
@@ -24,6 +58,7 @@ export default angular.module( name, [ angularMeteor, uiRouter ])
 
 function config($stateProvider) {
   'ngInject';
+  //Estados que acepta esta vista, su url (en la cual definimos un valor variable que será 'partidoId' supongo que de this) y sui template.
   $stateProvider.state('partidoView', {
     url: '/partidos/:partidoId',
     template: '<partido-view></partido-view>'
